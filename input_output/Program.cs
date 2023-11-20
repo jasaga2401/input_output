b9247a193f8b92a32fe9a4b15c896d734bdf2e90
jasaga2401 <jasaga@protonmail.com>
2023-11-20 13:04:12 +0000

    Add project files.

---
+++ input_output/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+namespace myApplication
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            // outputs text to the screen
+            Console.WriteLine("Hello you!");
+            Console.WriteLine("Very well thank you");
+            Console.WriteLine(5 + 5);
+
+            // outputs text on the same line
+            Console.Write("this will ..");
+            Console.Write("write this here .. ");
+
+            // declaring variables
+            string forename = "Peter";
+            string surname = "Smith";
+            int num = 100;
+            bool status = true;
+            double num_dec = 6.25;
+
+            const int con_num = 500;
+
+            // output the variable
+            Console.WriteLine();
+            Console.WriteLine("The number is " + con_num);
+        
+        }
+    }
+}
+
